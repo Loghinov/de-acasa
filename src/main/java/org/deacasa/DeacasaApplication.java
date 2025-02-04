ea0063e3086f0abd0f2f9d22d0d37374c6d4f0c5
Loghinov <loghinovsergiu@gmail.com>
2025-02-04 19:25:22 +0200

    Initial version

---
+++ src/main/java/org/deacasa/DeacasaApplication.java
@@ -0,0 +1,13 @@
+package org.deacasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeacasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeacasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/deacasa/DeacasaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deacasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeacasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
