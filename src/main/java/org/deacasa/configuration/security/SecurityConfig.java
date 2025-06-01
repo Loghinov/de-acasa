c5500b8e486594b9e8db1434bb52ec4dc232b313
Vladislav Odai <vladislav.odai@gmail.com>
2025-06-01 18:20:00 +0300

    first

--- src/main/java/org/deacasa/configuration/security/SecurityConfig.java
+++ src/main/java/org/deacasa/configuration/security/SecurityConfig.java
@@ -1,21 +1,15 @@
 package org.deacasa.configuration.security;
 
-
-
-import org.deacasa.repository.UserRepo;
-import org.deacasa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -41,7 +35,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/api/auth/register","/api/auth/login","/registration.html","/login.html","index.html","/home.html")
+                        .requestMatchers("/**")
                         .permitAll()
                        // .requestMatchers("/api/auth/token-verify-home").hasRole("ADMIN")
                         .anyRequest().authenticated())
